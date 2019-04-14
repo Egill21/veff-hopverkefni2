9b53ded8f58ebef9d95d2e446a757faf263f9174
Flow-flex-the-complex <fth29@hi.is>
2019-04-14 18:39:08 +0000

    Register og login komið sma

--- src/routes/login/Login.tsx
+++ src/routes/login/Login.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Input from './../../components/input/Input';
+import Button from './../../components/button/Button';
 
 import './Login.scss';
 
 export default function Login() {
+  function hallo() {
+    console.log('LOGIN');
+  }
   return (
-    <p>login</p>
+    <React.Fragment>
+      <h1>Innskráning</h1>
+      <Input text="Notendanafn:" type="text" name="userName" />
+      <Input text="Lykilorð:" type="password" name="password" />
+      <Button onClick={hallo}>Skrá inn</Button>
+      <Link to="/register">Nýskráning</Link>
+    </React.Fragment>
   );
 }
