74c0a507a8637bf6eb6e7135bdcaf1c5bc84cca3
Ásdís Erla <asdiserla@tg-dw103.rhi.hi.is>
2019-04-14 19:24:03 +0000

    byrja a login

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -18,70 +18,68 @@ export default function Products() {
   return (
     <Fragment>
       <div className="products__container">
-        <div className="products__content">
-          <div className="products__product">
-            <div className="products__box">
-              hallo
+        <div className="products__product">
+          <div className="products__box">
+            hallo
           </div>
-            <div className="products__info">
-              <div className="products__descr">
-                <h3 className="products__title">Ergonomic Rubber Bacon</h3>
-                <p>Sports</p>
-              </div>
-              <p className="products__price">635 kr.-</p>
+          <div className="products__info">
+            <div className="products__descr">
+              <h3 className="products__title">Ergonomic Rubber Bacon</h3>
+              <p>Sports</p>
             </div>
+            <p className="products__price">635 kr.-</p>
           </div>
+        </div>
 
-          <div className="products__product">
-            <div className="products__box">
-              hallo
+        <div className="products__product">
+          <div className="products__box">
+            hallo
           </div>
-            <div className="products__info">
-              <div className="products__descr">
-                <h3 className="products__title">Ergonomic Rubber Bacon</h3>
-                <p>Sports</p>
-              </div>
-              <p className="products__price">635 kr.-</p>
+          <div className="products__info">
+            <div className="products__descr">
+              <h3 className="products__title">Ergonomic Rubber Bacon</h3>
+              <p>Sports</p>
             </div>
+            <p className="products__price">635 kr.-</p>
           </div>
+        </div>
 
-          <div className="products__product">
-            <div className="products__box">
-              hallo
+        <div className="products__product">
+          <div className="products__box">
+            hallo
           </div>
-            <div className="products__info">
-              <div className="products__descr">
-                <h3 className="products__title">Ergonomic Rubber Bacon</h3>
-                <p>Sports</p>
-              </div>
-              <p className="products__price">635 kr.-</p>
+          <div className="products__info">
+            <div className="products__descr">
+              <h3 className="products__title">Ergonomic Rubber Bacon</h3>
+              <p>Sports</p>
             </div>
+            <p className="products__price">635 kr.-</p>
           </div>
+        </div>
 
-          <div className="products__product">
-            <div className="products__box">
-              hallo
+        <div className="products__product">
+          <div className="products__box">
+            hallo
           </div>
-            <div className="products__info">
-              <div className="products__descr">
-                <h3 className="products__title">Ergonomic Rubber Bacon</h3>
-                <p>Sports</p>
-              </div>
-              <p className="products__price">635 kr.-</p>
+          <div className="products__info">
+            <div className="products__descr">
+              <h3 className="products__title">Ergonomic Rubber Bacon</h3>
+              <p>Sports</p>
             </div>
+            <p className="products__price">635 kr.-</p>
           </div>
+        </div>
 
-          <div className="products__product">
-            <div className="products__box">
-              hallo
+        <div className="products__product">
+          <div className="products__box">
+            hallo
           </div>
-            <div className="products__info">
-              <div className="products__descr">
-                <h3 className="products__title">Ergonomic Rubber Bacon</h3>
-                <p>Sports</p>
-              </div>
-              <p className="products__price">635 kr.-</p>
+          <div className="products__info">
+            <div className="products__descr">
+              <h3 className="products__title">Ergonomic Rubber Bacon</h3>
+              <p>Sports</p>
             </div>
+            <p className="products__price">635 kr.-</p>
           </div>
         </div>
       </div>
--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -2,15 +2,42 @@ import React, { Fragment } from 'react';
 import Helmet from 'react-helmet';
 
 import Products from '../../components/products/Products';
+import Button from './../../components/button/Button';
 
 import './Home.scss';
 
 export default function Home() {
 
+  function hallo() {
+    console.log('hvað á að gerast hér?');
+  }
+
   return (
     <Fragment>
-      <Helmet title="Forsíða" />
-      <Products />
+      <div className="home">
+        <Helmet title="Forsíða" />
+        <h2 className="home__heading">Nýjar vörur</h2>
+        <Products />
+        <Button onClick={hallo} className="home__button">Skoða alla flokka</Button>
+        <h3 className="home__subheading">Skoðaðu vöruflokkana okkar</h3>
+        <div className="home__categories">
+          <div className="home__category">
+            <p className="category">Clothing</p>
+          </div>
+          <div className="home__category">
+            <p className="category">Shoes</p>
+          </div>
+          <div className="home__category">
+            <p className="category">Garden</p>
+          </div>
+          <div className="home__category">
+            <p className="category">Computers</p>
+          </div>
+          <div className="home__category">
+            <p className="category">Movies</p>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
