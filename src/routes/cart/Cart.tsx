25206a934c6c8fe16f21cd97ecf7656feeff9837
Ásdís Erla <aej25@hi.is>
2019-04-17 16:25:43 +0000

    baeta vid pontun

--- src/routes/cart/Cart.tsx
+++ src/routes/cart/Cart.tsx
@@ -11,7 +11,7 @@ export default function Cart() {
     <Context.Consumer>
       {({ token }) => {
         return (
-          <div className="cart">
+          <div className="cart__row">
             <AddToCart token={token} />
           </div>
         );
