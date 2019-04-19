4ca063c984deb5fe760b0e01e46b8698e35a4f2e
Flow-flex-the-complex <fth29@hi.is>
2019-04-19 16:03:46 +0000

    Verður rautt núna

--- src/routes/register/Register.tsx
+++ src/routes/register/Register.tsx
@@ -1,20 +1,23 @@
-import React, { Children, Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { Children, Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import { post2 } from "../../api/index";
-import { IFieldError } from "../../api/types";
-import Button from "./../../components/button/Button";
-import Input from "./../../components/input/Input";
+import { post2 } from '../../api/index';
+import { IFieldError } from '../../api/types';
+import Button from './../../components/button/Button';
+import Input from './../../components/input/Input';
 
-import "./Register.scss";
+import './Register.scss';
 
 export default function Register() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
   const [errors, setErrors] = useState<Array<IFieldError>>([]); // tslint:disable-line
 
   let status = 200;
+  let userError = false;
+  let passError = false;
+  let emailError = false;
 
   function changeUsername(e: React.ChangeEvent<HTMLInputElement>) {
     setUsername(e.target.value);
@@ -32,13 +35,13 @@ export default function Register() {
     const data = {
       username: username, // tslint:disable-line
       password: password, // tslint:disable-line
-      email: email, // tslint:disable-line
+      email: email // tslint:disable-line
     };
-    const response = await post2("users/register", data);
-    console.log("TCL: makeUser -> response", response); // tslint:disable-line
+    const response = await post2('users/register', data);
+    console.log('TCL: makeUser -> response', response); // tslint:disable-line
 
     status = response.status;
-    console.log("TCL: makeUser -> status", status); // tslint:disable-line
+    console.log('TCL: makeUser -> status', status); // tslint:disable-line
     if (status !== 200) {
       const final: IFieldError[] = response.response.errors;
       setErrors(final);
@@ -51,6 +54,13 @@ export default function Register() {
         <h1 className="register__title">Nýskráning</h1>
         {errors &&
           errors.map((i: IFieldError, index: number) => {
+            if (i.field === 'username') {
+              userError = true;
+            } else if (i.field === 'password') {
+              passError = true;
+            } else if (i.field === 'email') {
+              emailError = true;
+            }
             return (
               <label className="register__errors__item" key={index}>
                 {i.field}: {i.error}
@@ -63,25 +73,30 @@ export default function Register() {
             name="userName"
             type="text"
             text="Notendanafn:"
+            error={userError && errors.length !== 0}
           />
           <Input
             onChange={changePassword}
             name="password"
             type="password"
             text="Lykilorð:"
+            error={passError && errors.length !== 0}
           />
           <Input
             onChange={changeEmail}
             name="email"
             type="email"
             text="Netfang:"
+            error={emailError && errors.length !== 0}
           />
           <Button onClick={makeUser} className="register__button">
             Nýskrá
-        </Button>
+          </Button>
         </div>
       </div>
-      <Link to="/login" className="register__login">Innskráning</Link>
+      <Link to="/login" className="register__login">
+        Innskráning
+      </Link>
     </React.Fragment>
   );
 }
